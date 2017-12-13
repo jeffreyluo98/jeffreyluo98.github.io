1015d7b6acc53220126f463622f275341e674c0c
Zhili <zhililuo@wirelessprv-10-192-228-107.near.illinois.edu>
2017-12-13 12:56:58 -0600

    0.1
    0.1

---
+++ error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>RILO - Page not found</title>
+	<?php include("inc/htmlhead.php");?>
+</head>
+<body>
+<div class="container">
+<?php include("inc/header.php");?>
+<h3>Oops...seems you have entered a wrong address.</h3>
+<h4>Check your spelling. Beware of the capital letters.</h4>
+</div>
+</body>
+</html>
---
+++ inc/htmlhead.php
@@ -0,0 +1,6 @@
+<!-- Latest compiled and minified Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="js/script.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" media="screen" />
