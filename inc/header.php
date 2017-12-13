93c3cc4d4f3f764ee321638ce58e06e6bef3c251
Zhili <zhililuo@wirelessprv-10-192-228-107.near.illinois.edu>
2017-12-13 13:17:55 -0600

    1

--- inc/header.php
+++ inc/header.php
@@ -2,11 +2,6 @@
 <br/>
 <ul class="nav nav-tabs">
 	<li id="navbhome"><a href="index.php"><h2>Home</h2></a></li>
-	<li id="navbgym"><a href="gym.php"><h2>RILO Gym</h2></a></li>
-	<li id="navbtool"><a href="tools.php"><h2>Tools</h2></a></li>
+	<li id="navbproj"><a href="project.php"><h2>CS125 Project</h2></a></li>
 	<li id="navbabout"><a href="about.php"><h2>About</h2></a></li>
-	<li class="cms"><a href="#" onclick='alert("Coming Sooooooooon!");return false'><h2>Diet</h2></a></li>
-	<li class="cms"><a href="#" onclick='alert("Coming Sooooooooon!");return false'><h2>Workout Videos</h2></a></li>
-	<li class="cms"><a href="#" onclick='alert("Coming Sooooooooon!");return false'><h2>Forum</h2></a></li>
-	<li class="cms"><a href="#" onclick='alert("Coming Sooooooooon!");return false'><h2>Store</h2></a></li>
-</ul>
+	</ul>
