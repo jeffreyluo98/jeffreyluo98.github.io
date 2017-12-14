57671f449b73439a881dc12b6a4c1a900ce98ae8
Zhili <zhililuo@wirelessprv-10-192-228-107.near.illinois.edu>
2017-12-13 22:06:24 -0600

    Finally Done!!!

--- inc/header.php
+++
@@ -1,7 +0,0 @@
-<h1 id="logo">RILO</h1>
-<br/>
-<ul class="nav nav-tabs">
-	<li id="navbhome"><a href="index.php"><h2>Home</h2></a></li>
-	<li id="navbproj"><a href="project.php"><h2>CS125 Project</h2></a></li>
-	<li id="navbabout"><a href="about.php"><h2>About</h2></a></li>
-	</ul>
